75088c6623dd290ce6be58122b34793be59435ae
KhantMinSan <sinetio150@gmail.com>
2024-11-18 01:47:26 +0800

    modified BMI

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height, weight):
   print(f"Your BMI is {bmi}.")
   if(bmi<18.5):
     print("Under weight.")
+    return -1
   elif(18.5<=bmi<=25.0):
     print("Normal weight")
+    return 0
   else:
-    print("Over weight.")    
+    print("Over weight.")
+    return 1
 calculate_bmi(weight=57, height=1.73)
 
